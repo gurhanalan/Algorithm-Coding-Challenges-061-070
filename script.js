e61d01520243f59d79fc3591f6bc1f1958ca7110
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-04 19:28:42 +0300

    Add challenge 63

--- script.js
+++ script.js
@@ -35,3 +35,36 @@ function dataReverse(data) {
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // #############################################################
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// 62. Highest Scoring Word- 6 kyu - codewars
+/* 
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+ */
+function high(x) {
+    const alph = "abcdefghijklmnopqrstuvwxyz".split("");
+    const scoredX = x.split(" ").map((el) => {
+        let total = 0;
+        [...el].forEach((char) => {
+            total += alph.indexOf(char);
+        });
+        return total;
+    });
+
+    const highestScoreIdx = scoredX.indexOf(Math.max(...scoredX));
+
+    return x.split(" ")[highestScoreIdx];
+}
+
+console.log(high("man i need a taxi up to ubud"));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// #############################################################
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
