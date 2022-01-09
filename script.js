f3b47c61042ffee2f418b91449d8aba7c7706ebf
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-09 19:36:24 +0300

    Add challenge 68

--- script.js
+++ script.js
@@ -239,3 +239,64 @@ function highestRank(arr) {
 }
 
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// #############################################################
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// 68. Prime Numbers - 5kyu - Prime Numbers
+/* Preface
+A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+A more detailed description of prime numbers can be found at: http://en.wikipedia.org/wiki/Prime_number
+
+The Problem
+You will need to create logic for the following two functions: isPrime(number) and getPrimes(start, finish)
+
+isPrime(number)
+Should return boolean true if prime, otherwise false
+
+getPrimes(start, finish)
+Should return a unique, sorted array of all primes in the range [start, finish] (i.e. both numbers inclusive). Note that this range can go both ways - e.g. getPrimes(10, 1) should return all primes from 1 to 10 both inclusive.
+
+Sample Input:
+isPrime(number):
+
+isPrime(0); // === false
+isPrime(1); // === false
+isPrime(2); // === true
+isPrime(3); // === true
+isPrime(4); // === false
+isPrime(5); // === true 
+getPrimes(start, finish):
+
+getPrimes(0, 0); // === []
+getPrimes(0, 30); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+getPrimes(30, 0); // === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] */
+
+function isPrime(num) {
+    // check for 1 and 0
+    if (num <= 1) return false;
+
+    // make an array of prime numbers till the num
+    const arrPrimes = [2];
+    let i = 2;
+    //  decreasing the complexity of the algorithm from O(n) to O(sqrt(n))
+    while (i <= Math.sqrt(num)) {
+        if (num % i === 0) return false;
+
+        i++;
+    }
+    return true;
+}
+
+function getPrimes(x, y) {
+    const [start, finish] = x <= y ? [x, y] : [y, x];
+    const arr = Array(finish - start + 1)
+        .fill(0)
+        .map((_, i) => start + i);
+
+    return arr.filter((num) => isPrime(num));
+}
+
+console.log(getPrimes(0, 30));
