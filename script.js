7d329cf272870cf56de7401ef0224217b2232f93
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-11 19:38:59 +0300

    Add challenge 70

--- script.js
+++ script.js
@@ -318,3 +318,23 @@ function toCamelCase(str) {
         .replace(/(\W|_)(\w)/g, (x) => x.toUpperCase())
         .replace(/(\W|_)/g, "");
 }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// #############################################################
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 70. Kebabize - 6 kyu - codewars
+/* Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+kebabize('camelsHave3Humps') // camels-have-humps
+Notes:
+
+the returned string should only contain lowercase letters */
+
+function kebabize(str) {
+    return str
+        .replace(/([A-Z])/g, "-$&")
+        .toLowerCase()
+        .replace(/_/g, "-")
+        .replace(/\d/g, "");
+}
